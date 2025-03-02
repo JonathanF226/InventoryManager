043ca1ab8d879d4075762e568adedf275e7686c1
JonathanF226 <flores.jonathanqv@gmail.com>
2025-03-01 23:07:55 -0500

    Initial commit

---
+++ InventoryManager/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  InventoryManager
+//
+//  Created by Jonathan Flores on 3/1/25.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
