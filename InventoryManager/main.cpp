c1e20d1c5217b16327accb715f59ab7d310f7d55
JonathanF226 <flores.jonathanqv@gmail.com>
2025-03-04 23:30:26 -0500

    added item class

--- InventoryManager/main.cpp
+++ InventoryManager/main.cpp
@@ -7,7 +7,35 @@
 
 #include <iostream>
 
+using namespace std;
+
+class Item {
+private:
+    string name;
+    double price;
+    int stock;
+    
+public:
+    Item(string itemName, double itemPrice, int itemStock){
+        name = itemName;
+        price = itemPrice;
+        stock = itemStock;
+    }
+    
+    void addStock(int quantity){
+        stock += quantity;
+        cout << quantity << " item(s) added. New stock: " << stock << endl;
+    }
+    
+    void displayItem(){
+        cout << "Item: " << name << "\nPrice: $" << price << "\nStock: " << stock << endl;
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!\n";
+    Item apple("Apple", 0.99, 10);
+    apple.displayItem();
+    apple.addStock(10);
+    apple.displayItem();
     return 0;
 }
